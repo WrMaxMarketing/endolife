4b9d0db0a7fa2b31018d6b4e6b9bc4ac898aacfb
Victória Mendes <vick.costa1305@gmail.com>
2025-11-13 11:17:07 -0300

    correção de gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,13 +16,27 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       {/* Next.js gera o <head> automaticamente */}
-      <head />
+      <head>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id=GTM-52KGHST4'+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-52KGHST4');
+            `,
+          }}
+        />
+      </head>
       <body>
         {/* Google tag (gtag.js) — agora dentro do <body> */}
-        <Script
+        {/* <Script
           strategy="afterInteractive"
           async
-          src="https://www.googletagmanager.com/gtag/js?id=G-RCYT944HR4"
+          src="https://www.googletagmanager.com/gtag/js?id=G-52KGHST4"
         />
         <Script
           id="gtag-init"
@@ -32,11 +46,18 @@ export default function RootLayout({
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-RCYT944HR4');
+              gtag('config', 'G-52KGHST4');
+            `,
+          }}
+        /> */}
+        <div
+          dangerouslySetInnerHTML={{
+            __html: `
+              <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-52KGHST4"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
             `,
           }}
         />
-
         {children}
       </body>
     </html>
