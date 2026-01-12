b3fec3639e40b0edffc861b72b6181025232d797
WrMaxMarketing <marketingwrmax@gmail.com>
2026-01-12 09:03:31 -0300

    fix: corrigindo ordem dos imports no css

--- landing-page.tsx
+++ landing-page.tsx
@@ -268,16 +268,16 @@ export default function Component() {
                       </div>
                     </div>
 
-                    <div className="flex items-start space-x-4">
+                    {/*<div className="flex items-start space-x-4">
                       <div className="w-8 h-8 bg-[#A47FB5] rounded-full flex items-center justify-center flex-shrink-0 mt-1">
                         <Heart className="w-4 h-4 text-white" />
-                      </div>
-                      { /*  <div>
+                      </div>*/
+                    }  { /*  <div>
                         <p className="font-bold text-[#827C73] text-lg">Sessão estratégica</p>
                         <p className="text-gray-600">com nutricionista online</p>
-                      </div> */}
+                      </div> 
                     </div>
-
+*/}
                     <div className="flex items-start space-x-4">
                       <div className="w-8 h-8 bg-[#999C82] rounded-full flex items-center justify-center flex-shrink-0 mt-1">
                         <Users className="w-4 h-4 text-white" />
