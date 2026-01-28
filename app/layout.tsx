05d17ed3d75f4fd2319745ec89e1d36916353eb8
Victória Mendes <vick.costa1305@gmail.com>
2026-01-28 16:35:23 -0300

    add new gtags

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,47 +17,54 @@ export default function RootLayout({
     <html lang="pt-BR">
       {/* Next.js gera o <head> automaticamente */}
       <head>
+        {/* Meta Pixel */}
+        {/* <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '2612979692229075');
+              fbq('track', 'PageView');
+            `,
+          }}
+        /> */}
+
+
+        {/* Google Tag Manager */}
         <Script
-          id="gtm-script"
+          id="gtm"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id=GTM-52KGHST4'+dl;f.parentNode.insertBefore(j,f);
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer','GTM-52KGHST4');
             `,
           }}
         />
+        {/* End Google Tag Manager */}
       </head>
       <body>
-        {/* Google tag (gtag.js) — agora dentro do <body> */}
-        {/* <Script
-          strategy="afterInteractive"
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-52KGHST4"
-        />
-        <Script
-          id="gtag-init"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-52KGHST4');
-            `,
-          }}
-        /> */}
-        <div
-          dangerouslySetInnerHTML={{
-            __html: `
-              <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-52KGHST4"
-              height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-            `,
-          }}
-        />
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=GTM-52KGHST4`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
       </body>
     </html>
